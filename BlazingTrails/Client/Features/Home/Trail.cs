c3b785c31ab3af0556bc42fe3126478c7252b1b8
Vinh LN Nguyen <vinh-ngoc.nguyen@idealogic.com.vn>
2023-03-17 10:11:50 +0700

    init blazor example

---
+++ BlazingTrails/Client/Features/Home/Trail.cs
@@ -0,0 +1,15 @@
+namespace BlazingTrails.Client.Features.Home;
+
+public class Trail
+{
+    public int It { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public int TimeInMinutes { get; set; } = 0;
+    public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+    public int Length { get; set; }
+    public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+
+}
