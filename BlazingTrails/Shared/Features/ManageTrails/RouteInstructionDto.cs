8dada830b616541d676dcb60b28be4234c4c3d12
Vinh LN Nguyen <vinh-ngoc.nguyen@idealogic.com.vn>
2023-03-24 10:51:19 +0700

    Add and Configure API

---
+++ BlazingTrails/Shared/Features/ManageTrails/RouteInstructionDto.cs
@@ -0,0 +1,8 @@
+namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public class RouteInstructionDto
+{
+    public int Stage { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+}
---
+++ BlazingTrails/Shared/Features/ManageTrails/TrailDto.cs
@@ -0,0 +1,11 @@
+namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public class TrailDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public int TimeInMinutes { get; set; }
+    public int Length { get; set; }
+    public List<RouteInstructionDto> Routes { get; set; } = new();
+}
